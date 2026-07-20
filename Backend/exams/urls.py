a4ecdd672136fead37ca8cb281314d35a8ae2700
Raveena <raveenavarsha2002gmail.com>
2026-07-20 13:19:49 +0530

    Added users edit and delete in questions also added update and delete in exams

--- Backend/exams/urls.py
+++ Backend/exams/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateExamView, ListExamsView
+from .views import CreateExamView, ListExamsView, UpdateExamView, DeleteExamView
 
 urlpatterns = [
     path('create/', CreateExamView.as_view(), name='create-exam'),
     path('list/', ListExamsView.as_view(), name='list-exams'),
+    path('update/<int:exam_id>/', UpdateExamView.as_view(), name='update-exam'),
+    path('delete/<int:exam_id>/', DeleteExamView.as_view(), name='delete-exam'),
 ]
