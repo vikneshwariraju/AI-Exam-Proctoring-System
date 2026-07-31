dfeb38332da24a2d80fd6a387046d90d4d92f630
vikneshwariraju <vikneshwariraju@gmail.com>
2026-07-31 10:04:08 +0530

    resolved the faculty exam page

--- Frontend/src/services/resultsService.js
+++ Frontend/src/services/resultsService.js
@@ -1,18 +1,18 @@
 import api from "./authService";
 
-// Get all results of an exam (Faculty)
+// Get all results for a particular exam
 export const getExamResults = async (examId) => {
   const { data } = await api.get(`/results/faculty/exam-results/${examId}/`);
   return data;
 };
 
-// Publish one student's result
+// Publish one result
 export const publishResult = async (resultId) => {
   const { data } = await api.post(`/results/publish/${resultId}/`);
   return data;
 };
 
-// Publish all students' results
+// Publish all results of an exam
 export const publishAllResults = async (examId) => {
   const { data } = await api.post(`/results/publish-all/${examId}/`);
   return data;
